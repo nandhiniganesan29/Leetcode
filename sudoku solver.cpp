4af58c36523d1e9b93de485113201dfa70795d9f
nandhiniganesan29 <nandhiniganesan2914@gmail.com>
2026-03-14 20:29:55 +0530

    sudoku solver.cpp

---
+++ sudoku solver.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) return true;
+
+                            // Backtrack if no solution found
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false; // No valid number found for this cell
+                }
+            }
+        }
+        return true; // Board is full
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[i][col] == c) return false;
+            // Check column
+            if (board[row][i] == c) return false;
+            // Check 3x3 box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+};
