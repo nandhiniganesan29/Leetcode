90a73efe2ff128839e3d6f21bdecf5385eae8a0f
nandhiniganesan29 <nandhiniganesan2914@gmail.com>
2026-03-14 12:14:48 +0530

    rotated shorted.cpp

---
+++ rotated shorted.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Check if the left side is sorted
+            if (nums[left] <= nums[mid]) {
+                // Target is within the sorted left half
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } 
+            // Otherwise, the right side must be sorted
+            else {
+                // Target is within the sorted right half
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
