1335e4c5a19f702427cb138caf0de818e14ebb87
nandhiniganesan29 <nandhiniganesan2914@gmail.com>
2026-04-28 16:21:07 +0530

    unique path.js

---
+++ unique path.js
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        // 1. Fill the first row and first column with 1
+        // (Only one way to reach these cells: straight right or straight down)
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+        
+        // 2. Compute the rest of the grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
