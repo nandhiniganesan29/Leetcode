7d2a94da20fc366a0d4f561c76d7b6da261665d6
nandhiniganesan29 <nandhiniganesan2914@gmail.com>
2026-09-02 09:49:43 +0530

    palindrome linked list.cpp

---
+++ palindrome linked list.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return true;
+
+        // Find the middle of the linked list
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse the second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Compare first half and reversed second half
+        ListNode* left = head;
+        ListNode* right = prev;
+
+        while (right != nullptr) {
+            if (left->val != right->val)
+                return false;
+
+            left = left->next;
+            right = right->next;
+        }
+
+        return true;
+    }
+};
